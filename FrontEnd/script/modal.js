5fa2da6770e7b988d9a845562b351008d14c2960
Florian Poupon <pro.poupon.fl@gmail.com>
2025-07-02 15:43:44 +0200

    style : css modale

---
+++ FrontEnd/script/modal.js
@@ -0,0 +1,19 @@
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    // création des projets depuis API
+    const galleryModal = document.querySelector(".galleryModal");
+    data.forEach((work) => {
+      const projet = document.createElement("figure");
+      galleryModal.appendChild(projet);
+      const imgWork = document.createElement("img");
+      imgWork.src = work.imageUrl;
+      projet.appendChild(imgWork);
+      const trash = document.createElement("button");
+      trash.classList.add("deleteProject");
+      const i = document.createElement("i");
+      i.classList.add("fa-regular", "fa-trash-can");
+      trash.appendChild(i);
+      projet.appendChild(trash);
+    });
+  });
